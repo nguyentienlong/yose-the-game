1c62799c90c9f1f669996c5206123498c303233f
Anh Bui <bui.bao.anh@ipricegroup.com>
2017-03-06 15:22:41 +0800

    put ships in gates

--- resources/views/astroport/index.blade.php
+++ resources/views/astroport/index.blade.php
@@ -9,12 +9,15 @@
                 </div>
             </div>
             <div class="links">
-                <div id="gate-1">Gate 1</div>
-                <div id="gate-2">Gate 2</div>
-                <div id="gate-3">Gate 3</div>
-                <div id="ship-1">Ship 1</div>
-                <div id="ship-2">Ship 2</div>
-                <div id="ship-3">Ship 3</div>
+                <div id="gate-1">
+                    <div id="ship-1">Ship 1</div>
+                </div>
+                <div id="gate-2">
+                    <div id="ship-2">Ship 2</div>
+                </div>
+                <div id="gate-3">
+                    <div id="ship-3">Ship 3</div>
+                </div>
             </div>
         </div>
     </div>
