e46e4576ddd6146fa25bceca86acc39a9400bf5e
Ali Firash <guiburi@gmail.com>
2017-03-06 11:41:39 +0800

    hello yose

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Yo
+                    Hello Yose
                 </div>
 
                 <div class="links">
