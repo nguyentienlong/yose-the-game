0bd6c33ab087d2e53bb2b3b5e441e4b622cb6c10
Unknown <mahmoud.gamal.mousa@gmail.com>
2017-03-07 15:51:25 +0800

    add result

--- resources/views/primeFactors/index.blade.php
+++ resources/views/primeFactors/index.blade.php
@@ -2,19 +2,39 @@
 @extends('layout.master')
 
 @section('content')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript">
+    function getDecompostion(){
+        $.ajax({url: "/primeFactors?number="+$('#number').val(), success: function(result){
+                if( 'decomposition' in result){
+                    $('#result').html( result['decomposition'] );
+                }else if( 'error' in result){
+                    $('#result').html( 'error:' + result['error'] );
+                }else{
+                    $('#result').html( 'unkown error' );
+                }
+            }
+        });
+    }
+    </script>
+
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div id="title" class="title m-b-md">
                 Prime Factors
             </div>
             <div id="invitation">
-                Get the decomposed
+                Get the decompostion
             </div>
             <br />
-            <form action="/primeFactors" method="get">
+            <form action="#" method="get">
                 <input type="text" name="number" id="number">
-                <button id="go">GO!</button>
+                <button id="go" onclick="getDecompostion(); return false;">GO!</button>
             </form>
+            <br />
+            <br />
+            <div id="result">
+            </div>
         </div>
     </div>
 @endsection
