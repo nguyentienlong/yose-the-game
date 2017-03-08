cbdd506c1e4d261c76013a942ca4c1cc1365fc44
Ali Firash <guiburi@gmail.com>
2017-03-08 16:17:47 +0800

    p position

--- app/Http/Controllers/GeekController.php
+++ app/Http/Controllers/GeekController.php
@@ -8,6 +8,12 @@ class GeekController extends Controller
 {
     public function index(Request $request)
     {
+        $width = $request->get('width');
+        $map = $request->get('map');
+        $planePositionX = strpos($map,'P') % $width;
+        $planePositionY = ceil(strpos($map,'P') / $width);
+        var_dump($planePositionX,$planePositionY);
+
         dd(request()->all());
     }
 }
